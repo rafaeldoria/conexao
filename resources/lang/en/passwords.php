3fbf68811aca4a714b9ce966b68df1f9241dd90b
Rafael Dória <ieieldoria@hotmail.com>
2019-02-02 16:23:19 -0200

    #12 Tradução mensagens reset password.

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'As senha devem ter pelo menos 6 caracteres e corresponder a confirmação.',
+    'reset' => 'Sua senha foi alterada!',
+    'sent' => 'Enviamos um E-mail com seu link para redefinição de senha!',
+    'token' => 'Este token de redefinição de senha não é válido.',
+    'user' => 'Não encontramos um usuário com este endereço de e-mail',
 
 ];
