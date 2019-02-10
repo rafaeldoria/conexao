0ba39c3e0912e3d699f260ca26e68d660b40f4a4
Rafael Dória <ieieldoria@hotmail.com>
2019-02-10 20:47:51 -0200

    #22 Logs principais adicionados.

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Log extends Model
 {
     protected $fillable = [
-        'desc_log', 'type_log_id'
+        'desc_log', 'type_log_id', 'user_id'
     ];
 
     public function typeLog()
