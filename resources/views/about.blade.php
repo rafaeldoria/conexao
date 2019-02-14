38c653d671924a3e5415d48692a62980be35e124
Rafael Dória <ieieldoria@hotmail.com>
2019-02-14 16:22:17 -0200

    #29 About conexao.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,8 +15,25 @@
 
     @endsection
 
-    Conexão Nerd
-
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1 class="mb20">QUEM SOMOS</h1>
+            <div>
+                <p class="about_conexao">Então, o Conexão Nerd é uma idéia entre dois nerds viciados em games, bons filmes, super heróis, tecnologia.</br></br>
+                    Nerds esses que começaram a muito tempo esse vislumbre por toda esse mundo. Vindo de Cavaleiros do Zodíaco,
+                    passando por YuYu Hakusho, Naruto e continuando no mundo dos animes. Filmes sendo apresentados com verdadeiras
+                    lendas e marcos como Senhor dos Anéis, Harry Potter e hoje totalmente maravilhado com todos os filmes de 
+                    super heróis que temos. Em games meu amigos, são vários exemplos mas pra destacar, entre os dois criadores do 
+                    Conexão Nerd, são mais de 10 mil horas jogadas de Dota2 (hoooo).</br></br>
+                    É isso, o Conexão Nerd falará sobre esses assuntos em sua jornada ('Faça ou não faça. Tentativa não há.' Claro somos 
+                    fãs de Star Wars) de uma maneira bem simples, comentando como você ao conversar com um amigo. E contamos com a conexão 
+                    de todos para nos ajudar nessa jornada. 
+                    Sinta-se a vontade para criticar, elogiar, opniar, nos dar ideias, o importante para nós é realmente a Conexão Nerd. 
+                    Que a caminhada comece...
+                </p>
+            </div>
+        </div>
+    </div>
     <div class="social_icon_area clearfix">
         <div class="container">
             <div class="row">
